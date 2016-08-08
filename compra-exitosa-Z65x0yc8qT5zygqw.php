25101606bfe66afb08dd1638eb9944a827d42daf
fernando.bayley@gmail.com <fernando.bayley@gmail.com>
2016-08-08 00:28:47 -0300

    Se agrego pixel de seguimeinto Facebook a todas las paginas

--- compra-exitosa-Z65x0yc8qT5zygqw.php
+++ compra-exitosa-Z65x0yc8qT5zygqw.php
@@ -23,6 +23,26 @@
       <script src="components/html5shiv/html5shiv.js"></script>
   <![endif]-->
 
+    <!-- Facebook Pixel Code -->
+      <script>
+      !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+      n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+      document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+      fbq('init', '697092327092784');
+      fbq('track', "PageView");
+      fbq('track', 'ViewContent');
+      fbq('track', 'Search');
+      fbq('track', 'AddToWishlist');
+      fbq('track', 'Lead');
+      fbq('track', 'CompleteRegistration');</script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=697092327092784&ev=PageView&noscript=1"
+      /></noscript>
+    <!-- End Facebook Pixel Code -->
+
     <!-- Codigo de Tracking -->
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
